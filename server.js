064f433ac5c639d3cccc341e3f8c0070ea62ac57
Julien Knebel <julienknebel@gmail.com>
2013-03-14 23:26:17 +0100

    simple node server with connect

---
+++ server.js
@@ -0,0 +1,15 @@
+var connect = require('connect')
+  , http = require('http');
+
+var app = connect()
+  .use(connect.favicon())
+  .use(connect.logger('dev'))
+  .use(connect.static(__dirname))
+  .use(connect.directory(__dirname))
+  .use(connect.cookieParser())
+  .use(connect.session({ secret: 'my secret here' }))
+  .use(function(req, res){
+    res.end('Hello from Connect!\n');
+  });
+
+http.createServer(app).listen(3000);
