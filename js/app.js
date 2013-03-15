05955cdc03f1ada77e861ef0e5546086612520ae
jknebel <jknebel.prestataire@vsc-technologies.com>
2013-03-15 15:55:38 +0100

    model solved :D

--- js/app.js
+++ js/app.js
@@ -1,51 +1,66 @@
-window.App = Ember.Application.create({
-    LOG_TRANSITIONS: true
-});
+// first things first: let's create our app
+window.App = Ember.Application.create();
 
+// this is where we declare our routes
 App.Router.map(function(){
     this.resource("client");
 });
 
-App.ApplicationView = Ember.View.extend({
-    
-});
-
-App.ClientModel = Ember.Object.extend({
-    //id: null,
-    //name: null, 
-    //adress: null
-});
-App.ClientModel.reopen({
-    /*clients: [],
-    findAll: function(){
-        $.getJSON('/json/clients.json', function(data){
-            console.log(data);
-            clients.addObjects()
-            return data;
-        });
-    }*/
-});
-App.ClientController = Ember.ArrayController.extend({
-    //content: App.ClientModel,
-    //clients: App.ClientModel.findAll()
-});
-App.ClientView = Ember.View.extend({
-    findAll: function(){
-        $.getJSON('/json/clients.json', function(data){
-            console.log(data);
-            //clients.addObjects()
-            return data;
-        });
-    }, 
-    
-    didInsertElement: function(){
-        debugger;
-        console.log('ClientView loaded');
-        //this.findAll();
-        (function(){
-            $.getJSON('/json/clients.json', function(data){
-                console.log(data);
-            });
-        })();
+// we can customize what's happening when accessing the client route
+// http://emberjs.com/guides/routing/specifying-a-routes-model/
+App.ClientRoute = Ember.Route.extend({
+    model:function(){
+        return App.Context.get('clients');
     }
 });
+
+// let's declare a client class
+App.Client = Ember.Object.extend({
+    id: 0,
+    name: "", 
+    address: ""
+});
+// let's give it a static method so we can call it later
+// http://emberjs.com/guides/object-model/reopening-classes-and-instances/
+// here we create a findAll method, its goal is to retreive the datas from clients.json
+App.Client.reopenClass({
+    findAll: function(){
+        return $.getJSON('/json/clients.json').then(function(json){
+            var emberClients = [];
+            var len = json.length;
+            for (var i=0; i<len; i++){
+                emberClients.push( App.Client.create(json[i]) );
+            }
+            return emberClients;
+        });
+    }
+});
+
+App.Context = Ember.Object.create({
+   clients: []
+});
+
+// we need a controller to give datas to the view 
+// pay attention to the handlbars template : {{#each client in controller}}
+// the {{each}} loop (in the client template) is pointing to the ClientController
+App.ClientController = Ember.ArrayController.extend({
+    contentBinding:'App.Context.clients'
+});
+
+
+// App.ready will act as our init 
+App.ready = function(){
+    
+    // now when a user acces the app for the first time we need to call our Client.findAll() method
+    
+    App.Client.findAll()
+        .done(function(clients){
+            // when the AJAX call is done fill the clients property (of the context object) with our "ember converted" clients
+            App.Context.set('clients', clients);
+        })
+        .fail(function(err){
+            // something went wrong...
+            throw new Error( 'json file status is: ' + err.state() );
+        });
+    
+};
