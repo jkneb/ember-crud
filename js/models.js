5083bc0de93ab92405d5ceaaa459649322a59123
jknebel <jknebel.prestataire@vsc-technologies.com>
2013-03-25 10:18:03 +0100

    ember data 12 > 11

--- js/models.js
+++ js/models.js
@@ -1,9 +1,8 @@
 App.Store = DS.Store.extend({
-    revision: 12, 
+    revision: 11, 
     adapter: 'DS.FixtureAdapter'
 });
 
-// let's declare the user class
 App.User = DS.Model.extend({
     id     : DS.attr('number'),
     name   : DS.attr('string'),
@@ -14,24 +13,24 @@ App.User = DS.Model.extend({
 
 App.User.FIXTURES = [
     {
-        id: "1",
-        name: "Julien Knebel",
-        email: "julienknebel@gmail.com",
-        bio : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        avatar : "/images/avatars/jk.jpg"
+        id: 1,
+        name: 'Julien Knebel',
+        email: 'julienknebel@gmail.com',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        avatar: '/images/avatars/jk.jpg'
     }, 
     {
-        id: "2",
-        name: "Sponge Bob",
-        email: "bob@sponge.com",
-        bio: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        avatar: "/images/avatars/bs.jpg"
+        id: 2,
+        name: 'Sponge Bob',
+        email: 'bob@sponge.com',
+        bio: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        avatar: '/images/avatars/bs.jpg'
     },
     {
-        id: "3",
-        name: "John Doe",
-        email: "john@doe.com",
-        bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        avatar: "/images/avatars/jd.jpg"
+        id: 3,
+        name: 'John Doe',
+        email: 'john@doe.com',
+        bio: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        avatar: '/images/avatars/jd.jpg'
     }
 ];
