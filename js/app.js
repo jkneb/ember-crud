26e460506b91e22456d751d23beaeacf2e418e82
Julien Knebel <julienknebel@gmail.com>
2013-03-27 01:11:37 +0100

    comments session

--- js/app.js
+++ js/app.js
@@ -5,15 +5,19 @@ window.App = Ember.Application.create({
 
 // this is where we declare our routes
 App.Router.map(function(){
+    // this route will be our list of all users
     this.resource('users', function(){
+        // this one is nested and dynamic we need it to see one user at a time with its id
         this.resource('user', { path:'/:user_id' }, function(){
+            // and another nested one for editing the current user
             this.route('edit');
         });
+        // and finally the create route nested in users
         this.route('create');
     });
 });
 
-// no need of a home page
+// no need of a home page so we redirect "/" to "/users"
 App.IndexRoute = Ember.Route.extend({
     redirect: function(){
         this.transitionTo('users');
@@ -21,6 +25,7 @@ App.IndexRoute = Ember.Route.extend({
 });
 
 // we can customize what's happening when accessing the user route
+// here we simply retreive datas from our model and assign it to the usersRoute model
 // http://emberjs.com/guides/routing/specifying-a-routes-model/
 App.UsersRoute = Ember.Route.extend({
     model: function(){
@@ -28,13 +33,23 @@ App.UsersRoute = Ember.Route.extend({
     }
 });
 
+// we would need to define model for our nested/dynamic userRoute 
+// but by following Ember's naming conventions we get it for free 
+// in fact we don't even need to declare the userRoute 
+/*App.UserRoute = Ember.Route.extend();*/
 
 
-// pay attention to the handlbars data-template-name="users" template 
-// see the {{#each user in controller}} tag? In this case controller = UsersController
+// the users route will render a list of users so we need an ArrayController
+// and also pay attention to the handlbars users template 
+// you'll see the {{#each user in controller}} which starts the loop 
+// well in that particular case controller means UsersController
 App.UsersController = Ember.ArrayController.extend();
 
+// our nested user route will render only a single user at a time 
+// so in this case we'll use an ObjectController
 App.UserController = Ember.ObjectController.extend({
+    // the property isEditing is also used in the user template 
+    // this will let us switch class names if false or true
     isEditing: false, 
     
     edit: function(){
