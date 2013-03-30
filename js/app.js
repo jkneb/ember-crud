b7ce4daad6fc01c4f27de2228fde0dd852c180db
Julien Knebel <julienknebel@gmail.com>
2013-03-30 02:02:56 +0100

    comment

--- js/app.js
+++ js/app.js
@@ -98,6 +98,7 @@ App.UserEditController = Ember.ObjectController.extend({
     // we want this controller to inherit from its parent controller 
     // in this case it's userController 
     // http://emberjs.com/guides/controllers/dependencies-between-controllers/ 
+    // http://darthdeus.github.com/blog/2013/01/27/controllers-needs-explained/ 
     needs: ['user'], 
     
     // in the template we used a {{action closeEditing}} tag wich will trigger this method on click 
