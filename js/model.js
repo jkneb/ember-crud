9f046838403e380f8c1063392414d24129a9df09
Julien Knebel <julienknebel@gmail.com>
2013-04-01 04:50:24 +0200

    add dean user

--- js/model.js
+++ js/model.js
@@ -22,7 +22,7 @@ App.User.FIXTURES = [
         id: 2,
         name: 'Sponge Bob',
         email: 'bob@sponge.com',
-        bio: 'Duis aute irure dolor in reprehenderit in voluptate fugiat nulla pariatur.',
+        bio: 'Lorem ispum dolor sit amet in voluptate fugiat nulla pariatur.',
         avatarUrl: '/images/avatars/sb.jpg'
     },
     {
@@ -36,14 +36,21 @@ App.User.FIXTURES = [
         id: 4,
         name: 'Jane Doe',
         email: 'jane@doe.com',
-        bio: 'Duis aute irure in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        bio: 'Duis aute irure in voluptate velit esse cillum nulla pariatur.',
         avatarUrl: '/images/avatars/default.png'
     },
     {
-        id: 5,
+        id: 6,
+        name: 'Dean Winchester',
+        email: 'deany@plopmail.com',
+        bio: ':)',
+        avatarUrl: '/images/avatars/dean.jpg'
+    },
+    {
+        id: 7,
         name: 'John Doe',
         email: 'john@doe.com',
-        bio: 'Sed do eiusmod tempor in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        bio: 'Sed do eiusmod tempor velit esse cillum dolore eu fugiat pariatur.',
         avatarUrl: '/images/avatars/default.png'
     }
 ];
