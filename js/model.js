c464459771be6c852d73bcc496acc51da8858ee5
jknebel <jknebel.prestataire@vsc-technologies.com>
2013-04-02 14:42:17 +0200

    refacto

--- js/model.js
+++ js/model.js
@@ -16,41 +16,41 @@ App.User.FIXTURES = [
         name: 'Julien Knebel',
         email: 'julienknebel@gmail.com',
         bio: 'Freelance web & print designer + front-end developer',
-        avatarUrl: '/images/avatars/jk.jpg'
+        avatarUrl: './images/avatars/jk.jpg'
     }, 
     {
         id: 2,
         name: 'Sponge Bob',
         email: 'bob@sponge.com',
         bio: 'Lorem ispum dolor sit amet in voluptate fugiat nulla pariatur.',
-        avatarUrl: '/images/avatars/sb.jpg'
+        avatarUrl: './images/avatars/sb.jpg'
     },
     {
         id: 3,
         name: 'John Doe',
         email: 'john@doe.com',
         bio: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        avatarUrl: '/images/avatars/default.png'
+        avatarUrl: './images/avatars/default.png'
     },
     {
         id: 4,
         name: 'Jane Doe',
         email: 'jane@doe.com',
         bio: 'Duis aute irure in voluptate velit esse cillum nulla pariatur.',
-        avatarUrl: '/images/avatars/default.png'
+        avatarUrl: './images/avatars/default.png'
     },
     {
         id: 6,
         name: 'Dean Winchester',
         email: 'deany@plopmail.com',
         bio: ':)',
-        avatarUrl: '/images/avatars/dean.jpg'
+        avatarUrl: './images/avatars/dean.jpg'
     },
     {
         id: 7,
         name: 'John Doe',
         email: 'john@doe.com',
         bio: 'Sed do eiusmod tempor velit esse cillum dolore eu fugiat pariatur.',
-        avatarUrl: '/images/avatars/default.png'
+        avatarUrl: './images/avatars/default.png'
     }
 ];
