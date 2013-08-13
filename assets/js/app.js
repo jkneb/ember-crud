124830eb0d9c42c540903e4d0040425e8d58dce9
Julien Knebel <julienknebel@gmail.com>
2013-08-14 00:28:41 +0200

    compil

--- assets/js/app.js
+++ assets/js/app.js
@@ -254,6 +254,9 @@ App.ApplicationRoute = Em.Route.extend({
     events: {
         showModal: function(name){
             this.controllerFor(name).set('modalVisible', true);
+        }, 
+        customTransitionTo: function(name){
+            this.transitionTo(name);
         }
     }
 });
@@ -679,6 +682,91 @@ App.ConfirmDeleteButtonView = Ember.View.extend({
         }, 500);
     }
 });
+App.DraggableView = Em.View.extend({
+
+    // touch gestures properties
+    startX      : 0,
+    dist        : 0,
+    active      : null,
+    maxDist     : 250,
+    threshold   : 40,
+    activeWidth : null,
+
+    touchStart: function(event){
+        var touchEvent = event.originalEvent.changedTouches[0];
+        var layer = $(touchEvent.target).closest('.pane')[0];
+        if (layer) {
+            this.active = layer;
+            this.onStart(event, touchEvent);
+            this.activeWidth = $('.pane').outerWidth();
+        }
+    },
+
+    touchMove: function(event){
+        event.preventDefault();
+        if (!this.active) return;
+        this.onMove(event, event.originalEvent.changedTouches[0]);
+    },
+
+    touchEnd: function(event){
+        if (!this.active) return;
+        this.onEnd(event);
+    },
+
+    onStart: function(e,d) {
+        e.stopPropagation();
+        this.startX = d.pageX;
+        this.active.classList.add('drag');
+    },
+
+    onMove: function(e,d) {
+        e.stopPropagation();
+        this.dist = (d.pageX - this.startX) / 2;
+
+        // drag ⇚
+        if (this.dist > 0) { 
+            this.active.style.webkitTransform = 'translate3d(' + (-this.activeWidth + this.dist) + 'px, 0, 0)';
+        } 
+        // drag ⇛
+        else { 
+            this.active.style.webkitTransform = 'translate3d(' + (-this.activeWidth + this.dist) + 'px, 0, 0)';
+        }
+    },
+
+    onEnd: function(e) {
+        e.stopPropagation();
+        this.active.classList.remove('drag');
+
+        // dragged ⇛
+        if (this.dist >= this.threshold) { 
+            //console.log('⇛');
+            this.active.style.webkitTransform = 'translate3d(0px, 0, 0)';
+            this.active.classList.remove('active');
+            //this.active.style.webkitTransform = 'none';
+            
+            Em.run.later(this, function(){
+                // there is no customTransitionTo in the controller so it will bubble up to routes
+                // the customTransitionTo event is located in the ApplicationRoute
+                debugger;
+                this.get('controller').send('customTransitionTo', (this.renderedName === 'user') ? 'users' : 'index');
+            }, 600);
+        }
+        // cancel
+        else if (this.dist > -this.threshold && this.dist < this.threshold) { 
+            this.active.style.webkitTransform = 'translate3d(' + -(this.activeWidth) + 'px, 0, 0)';
+        } 
+        // dragged ⇚
+        else { 
+            //console.log('⇚');
+            //console.log('canceled');
+            this.active.style.webkitTransform = 'translate3d(' + -(this.activeWidth) + 'px, 0, 0)';
+        }
+
+        //this.active.removeAttribute('style');
+        this.dist = 0;
+        this.active = null;
+    }
+});
 App.Modal = Em.View.extend({
     layoutName: 'modal_layout',
 
@@ -695,95 +783,14 @@ App.Modal = Em.View.extend({
     }
 });
 
-App.UserEditView = Ember.View.extend({
-    sendCloseEditEvent: function(){
-        
-    }
-});
-App.UserView = Ember.View.extend({
-
-    // touch gestures properties
-    startX      : 0,
-    dist        : 0,
-    active      : null,
-    threshold   : 40,
+// inherit from the DraggableView
+App.UserEditView = App.DraggableView.extend();
+// inherit from the DraggableView
+App.UserView = App.DraggableView.extend({
 
     didInsertElement: function(){
-        // the didInsertElement hook is the guarantee 
-        // that the view is in the DOM. 
+        // the didInsertElement hook is the guarantee that the view is in the DOM. 
         // So from here, you can perform any DOM manipulation 
-        // or what ever you wish with or without jQuery.
-    }, 
-    
-    touchStart: function(event){
-        var touchEvent = event.originalEvent.changedTouches[0];
-        console.log($(touchEvent.target)[0]);
-        var layer = $(touchEvent.target).closest('.user-edit')[0];
-        if (layer) {
-            this.active = layer;
-            this.onStart(event, touchEvent);
-        }
-    },
-    
-    touchMove: function(event){
-        event.preventDefault();
-        if (!this.active) return;
-        this.onMove(event, event.originalEvent.changedTouches[0]);
-    },
-    
-    touchEnd: function(event){
-        if (!this.active) return;
-        this.onEnd(event);
-    }, 
-    
-    onStart: function(e,d) {
-        e.stopPropagation();
-        this.startX = d.pageX;
-        this.active.classList.add('drag');
-    },
-
-    onMove: function(e,d) {
-        e.stopPropagation();
-        this.dist = (d.pageX - this.startX) / 2;
-        var pct, r;
-        
-        this.active.classList.remove('webkit-transform');
-        
-        // drag from left to right
-        if (this.dist > 0) { 
-            this.active.style.webkitTransform = 'translate3d(' + this.dist + 'px, 0, 0)';
-        } 
-        else { // drag from right to left
-            this.active.style.webkitTransform = 'translate3d(' + this.dist + 'px, 0, 0)';
-        }
-    },
-
-    onEnd: function(e) {
-        e.stopPropagation();
-        this.active.classList.remove('drag');
-
-        // dragged from left to right
-        if (this.dist >= this.threshold) { 
-            this.active.classList.add('webkit-transform');
-            this.active.style.webkitTransform = 'translate3d(' + $('.user-edit').outerWidth() + 'px, 0, 0)';
-            this.active.classList.remove('active');
-
-            this.sendCloseEvent();
-        } 
-        // cancel
-        else if (this.dist > -this.threshold && this.dist < this.threshold) { 
-            this.active.classList.add('webkit-transform');
-            this.active.style.webkitTransform = 'translate3d(0px, 0, 0)';
-        } 
-        // dragged from right to left
-        else { 
-            console.log('canceled');
-            
-            this.active.classList.add('webkit-transform');
-            this.active.style.webkitTransform = 'translate3d(0px, 0, 0)';
-        }
-        
-        this.dist = 0;
-        this.active = null;
-    }    
+        // or what ever you want with or without jQuery.
+    }
 });
