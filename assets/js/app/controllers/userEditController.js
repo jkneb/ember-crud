5296ae2e6dc7a8c4390fec98cf4ae339b4c5631a
jknebel <jknebel.prestataire@vsc-technologies.com>
2013-08-14 18:24:38 +0200

    refacto

--- assets/js/app/controllers/userEditController.js
+++ assets/js/app/controllers/userEditController.js
@@ -1,5 +1,5 @@
 App.UserEditController = Ember.ObjectController.extend({
-    // we want this controller to inherit from its parent controller 
+    // we want this controller to inherit from another controller 
     // in this case it's userController 
     // http://emberjs.com/guides/controllers/dependencies-between-controllers/ 
     // http://darthdeus.github.com/blog/2013/01/27/controllers-needs-explained/ 
