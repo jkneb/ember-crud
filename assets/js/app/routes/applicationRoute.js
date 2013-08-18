898dba734f201fd2498c64e76bddb6378a4d5914
Julien Knebel <julienknebel@gmail.com>
2013-08-18 20:03:12 +0200

    rollback to a css transitions approach for the mobile version

--- assets/js/app/routes/applicationRoute.js
+++ assets/js/app/routes/applicationRoute.js
@@ -2,6 +2,9 @@ App.ApplicationRoute = Em.Route.extend({
     events: {
         showModal: function(name){
             this.controllerFor(name).set('modalVisible', true);
+        },
+        goBack: function(){
+            this.transitionTo('users');
         }
     }
 });
