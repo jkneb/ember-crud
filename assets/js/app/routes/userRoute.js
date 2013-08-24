009d87b5583de4deff7993514d34397c46da2a47
mbreton <mbreton@xebia.fr>
2013-08-24 17:01:09 +0200

    Add comment about controller.set('model' ...)

--- assets/js/app/routes/userRoute.js
+++ assets/js/app/routes/userRoute.js
@@ -8,7 +8,12 @@ App.UserRoute = Ember.Route.extend({
     setupController: function(controller, model){
         // force the deleteMode to false when accessing user
         this.controllerFor('user').set('deleteMode', false);
-        // have no fucking idea why I have to do this
+
+        // when we override the setupController, we disabled the default behavior
+        // of the Ember Route about the automatic model registration : By default the
+        // Ember route will save automaticly the model (the object passed by a transitionTo
+        // or, returned by the model method of the route) into a 'model' variable in the Controller.
+        // So to keep the fonctionnality after overriding you must implement it yourself.
         controller.set('model', model);
     },
 
