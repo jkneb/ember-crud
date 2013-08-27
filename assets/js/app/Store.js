1ae1641c2eb8ec066782bcc6c734bb6b30c098fc
Julien Knebel <julienknebel@gmail.com>
2013-08-27 13:36:44 +0200

    add file Store.js

---
+++ assets/js/app/Store.js
@@ -0,0 +1,6 @@
+App.Store = DS.Store.extend({
+    /* adapter: 'DS.FixtureAdapter' */
+    
+    // in this demo we are using the LocalStorageAdapter to persist data
+    adapter: 'DS.LSAdapter'
+});
