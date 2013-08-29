eeef4540be57c9f99a33f4b2033ca99143efd6f7
mbreton <mbreton@xebia.fr>
2013-08-29 22:25:16 +0200

    Comment integration test
    Add a waitfor helper

--- tests/integration/tests-integration-utils.js
+++ tests/integration/tests-integration-utils.js
@@ -1,5 +1,36 @@
+Ember.Test.registerHelper('waitFor', function (app, testFx, onReady, onTimeout, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000; //< Default Max Timout is 3s
+    var start = new Date().getTime();
+    var condition = false;
+
+    Em.run.begin();
+    var interval = setInterval(function() {
+        if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+            // If not time-out yet and condition not yet fulfilled
+            condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+        } else {
+            if(!condition) {
+                // If condition still not fulfilled (timeout but condition is 'false')
+                typeof(onTimeout) === "string" ? eval(onTimeout) : onTimeout(); //< Do what it's supposed to do once the condition is fulfilled
+            } else {
+                // Condition fulfilled (timeout and/or condition is 'true')
+                console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+            }
+            clearInterval(interval);
+            Em.run.end();
+        }
+    }, 250); //< repeat check every 250ms
+
+    return wait(app);
+});
+
 // inject many helpers usefull for the integration tests
 App.injectTestHelpers();
+
+App.Store = DS.Store.extend({
+     adapter: 'DS.FixtureAdapter'
+});
 
 // Global Integration tests beforeEach
 beforeEach(function(){
