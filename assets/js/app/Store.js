769014fc71081c6957e64914d3d1c0a9ea3c2e74
Julien Knebel <julienknebel@gmail.com>
2013-09-01 23:08:05 +0200

    Upgrade to ember 1.0.0 + Ember-Data 1.0 beta1 + LSAdapter update But createRecord from UsersCreateController is still broken

--- assets/js/app/Store.js
+++ assets/js/app/Store.js
@@ -1,6 +1,6 @@
 App.Store = DS.Store.extend({
-    /* adapter: 'DS.FixtureAdapter' */
+    /* adapter: DS.FixtureAdapter */
     
     // in this demo we are using the LocalStorageAdapter to persist data
-    adapter: 'DS.LSAdapter'
+    adapter: DS.LSAdapter
 });
--- assets/js/app/controllers/UserController.js
+++ assets/js/app/controllers/UserController.js
@@ -16,7 +16,7 @@ App.UserController = Ember.ObjectController.extend({
         confirmDelete: function(){
             // delete a user
             this.get('content').deleteRecord();
-            this.get('store').commit();
+            this.get('model').save();
             // then transition to the UsersRoute
             this.transitionToRoute('users');
         }
