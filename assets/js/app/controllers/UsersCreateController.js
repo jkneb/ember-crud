1ce8593b0b86fd9139fcf2e6f7c5e85bae41aef4
Julien Knebel <julienknebel@gmail.com>
2013-09-02 00:55:01 +0200

    better fix for the UsersCreateRoute

--- assets/js/app/controllers/UsersCreateController.js
+++ assets/js/app/controllers/UsersCreateController.js
@@ -5,8 +5,9 @@ App.UsersCreateController = Ember.ObjectController.extend({
         save: function () {
             // just before saving, we set the creationDate
             this.get('model').set('creationDate', new Date());
-            // save and commit
-            this.store.createRecord('user', this.get('model'));
+            // create a new user and save it
+            var newUser = this.store.createRecord('user', this.get('model'));
+            newUser.save();
 
             // redirects to the user itself
             this.transitionToRoute('user', this.get('model'));
