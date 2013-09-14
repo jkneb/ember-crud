2d07e8d4d12827fe9de10effc14a90a67290c3a4
Julien Knebel <julienknebel@gmail.com>
2013-09-14 23:57:57 +0200

    App.ModalView instead of App.Modal

--- assets/js/app/views/ModalView.js
+++ assets/js/app/views/ModalView.js
@@ -1,5 +1,5 @@
-App.Modal = Em.View.extend({
 // the modal view is a layout, take a look at the modal_layout template it has a {{yield}} where content can be encapsulated by the ModalView
+App.ModalView = Em.View.extend({
     layoutName: 'modal_layout',
 
     // each modal has a hideModal method triggered from inside the modal_layout template
